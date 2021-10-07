6d6b81452d58557cc3f9e92ea7dd0be06348ea11
Larry <olalerulanre@gmail.com>
2021-10-07 21:10:17 +0100

    - logout

--- src/redux/sagas/mySagas.js
+++ src/redux/sagas/mySagas.js
@@ -1,5 +1,5 @@
 import { call, put, takeEvery } from "redux-saga/effects";
-import { DATA_SAVED, USER_CREATED, USER_LOGIN } from "../types";
+import { DATA_SAVED, USER_CREATED, USER_LOGIN, USER_SIGN_OUT } from "../types";
 
 function* saveUserInfo({ payload }) {
   // save token, user id
@@ -8,8 +8,15 @@ function* saveUserInfo({ payload }) {
   yield put({ type: DATA_SAVED, payload })
 }
 
+function* clearUserBrowser() {
+  localStorage.removeItem("userId")
+  localStorage.removeItem("authorId")
+  localStorage.removeItem("token")
+}
+
 function* watcherSaga() {
   yield takeEvery([USER_LOGIN, USER_CREATED], saveUserInfo)
+  yield takeEvery(USER_SIGN_OUT, clearUserBrowser)
 }
 
 export default watcherSaga;
